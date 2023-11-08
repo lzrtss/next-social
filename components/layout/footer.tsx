ee8a1f248ac62084c1030eda560a72a8f3805272
Sergei <mac@MacBook-Air-Serhii.local>
2023-11-08 13:39:18 +0200

    Added footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
+
 import { Container } from '@/components';
 
 export default function Footer() {
   return (
     <footer className="hidden md:block w-full border-t border-t-neutral-700 bg-neutral-800 z-20">
-      <Container className="max-w-7xl flex items-center justify-between gap-3 xs:gap-5">
-        Footer
+      <Container className="max-w-7xl flex justify-center items-center gap-1">
+        <p className="text-sm text-neutral-400">
+          Copyright &copy; {new Date().getFullYear()}{' '}
+          <Link href="/" className="hover:underline underline-offset-2">
+            Thoughts
+          </Link>
+          . All rights reserved.
+        </p>
       </Container>
     </footer>
   );
